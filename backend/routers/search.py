f506fe2f93c3d36ab9118b77f9f9bae292b75ec3
tungnguyentu <tungnguyentu@bizflycloud.vn>
2022-04-07 15:30:11 +0700

    update frontend, advanced search

--- backend/routers/search.py
+++ backend/routers/search.py
@@ -1,5 +1,8 @@
+import re
+
 from flask import request, render_template, Blueprint
 from backend.utils import _get_collection, get_result
+from backend.settings import Config
 
 search_bp = Blueprint('search', __name__)
 
@@ -10,23 +13,32 @@ def index():
     return render_template('search.html')
 
 
-@search_bp.route("/search", methods=['POST'])
+@search_bp.route('/search', methods=['POST'])
 def get_info_by_mail_from():   
-    limit = 30
-    email = request.form.get("email")
-    if not email:
-        return {"error": "Email is required"}
-    email = f"<{email.strip()}>"
-    collection = _get_collection()
+    limit = 10
+    data = request.form
+    email_from = f"<{data.get('email_from')}>"
     filter = {
-        'Status': 'bounced', 
-        'From': f'{email}'
+        'From': f'{email_from}',
     }
+
+    email_to = data.get('email_to')
+    if email_to:
+        email_to = f"<{data.get('email_to')}>"
+        filter.update({'To': f'{email_to}'})
+
+    regx = re.compile(r'^.*(\breputation\b|\bspam\b|\bspamhaus\b|\blisted\b|\bblock\b|\bblocked\b|\bsecurity\b|\bblacklisted\b|\bphish\b|\bphishing\b|\bvirus\b|\bblacklisted\b|\bblacklist\b).*($|[^\w])')
+    spam = data.get('spam')
+    if spam:
+        spam_filter = {"Message": regx}
+        filter.update(spam_filter)
+    
     sort = list({'SentAt': -1}.items())
-    records = collection.find(
+    collection = _get_collection()
+    query = collection.find(
         filter = filter,
         sort = sort,
         limit = limit
     )
-    result = get_result(records)
-    return render_template('search.html', results=result["records"])
+    result = get_result(query)
+    return render_template('result.html', results=result["records"])
